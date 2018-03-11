911889436e91f8c9a17974b1a9ef616d1d8987a1
ankurmongodb <ankur.raina@mongodb.com>
2018-03-11 09:37:54 +0530

    forking repo requires internal code changes

--- trackerapi/me.go
+++ trackerapi/me.go
@@ -8,8 +8,8 @@ import (
 	"os"
 	u "os/user"
 
-	"github.com/GoBootcamp/clirescue/cmdutil"
-	"github.com/GoBootcamp/clirescue/user"
+	"github.com/Ankur10gen/clirescue/cmdutil"
+	"github.com/Ankur10gen/clirescue/user"
 )
 
 var (
